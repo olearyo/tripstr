784c1a134632a6ad93ca03d7eca274cf1fb01413
Drashya Gohil <54857785+drashya-codes@users.noreply.github.com>
2019-11-29 01:43:39 -0500

    Updated view-trip details without Join

--- dashboard/view-trip-details.php
+++ dashboard/view-trip-details.php
@@ -20,86 +20,113 @@ $accomDetailsTable -> execute();
 $transptIdTable = $pdo->prepare("SELECT * FROM `transportation` WHERE `tripId` = $tripId;");
 $transptIdTable -> execute();
 
-$eventIdTable = $pdo->prepare("SELECT * FROM `events` WHERE `eventId` = $tripId;");
-$eventIdTable -> execute();
+$eventsIdTable = $pdo->prepare("SELECT * FROM `events` WHERE `tripId` = $tripId;");
+$eventsIdTable -> execute();
+
+$othersIdTable = $pdo->prepare("SELECT * FROM `others` WHERE `tripId` = $tripId;");
+$othersIdTable -> execute();
 
 echo("<a href=show-trips-dashboard.php> Back to Dashboard </a>");
 echo("<br><br>");
 
 while($tripsRow = $tripsTable->fetch()) {
-    while($accomRow = $accomIdTable ->fetch()){
-        echo('<div>');
-            
-        echo("<h2>");
-        echo($tripsRow["tripName"]);
-        echo("</h2>");
+    while($transptRow = $transptIdTable->fetch()){
+        while($accomRow = $accomIdTable ->fetch()){
+            while($eventsRow = $eventsIdTable->fetch()){
+                while($othersRow = $othersIdTable->fetch()){
+                    echo('<div>');
+                        
+                    echo("<h2>");
+                    echo($tripsRow["tripName"]);
+                    echo("</h2>");
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SHOW ACCOMMODATION DETAILS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-        $accomDetailsTable = $accomDetailsTable -> fetch();
-        echo("<h3>");
-        echo("Staying at: ".$accomDetailsTable["name"]);
-        echo("</h3>");
-        echo("<br>");
-        echo("Booking ID: ".$accomDetailsTable["bookingId"]);
-        echo("<br><br>");
-        echo("Address: ".$accomDetailsTable["address"]);
-        echo("<br><br>");
+                    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SHOW ACCOMMODATION DETAILS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+                    $accomDetailsTable = $accomDetailsTable -> fetch();
+                    echo("<h3>");
+                    echo("Staying at: ".$accomDetailsTable["name"]);
+                    echo("</h3>");
+                    echo("<br>");
+                    echo("Booking ID: ".$accomDetailsTable["bookingId"]);
+                    echo("<br><br>");
+                    echo("Address: ".$accomDetailsTable["address"]);
+                    echo("<br><br>");
 
-        //show accommodation CheckIn and CheckOut Dates
-        echo("Check In: ".$accomDetailsTable["checkIn"]);
-        echo(" | ");
-        echo("Check Out: ".$accomDetailsTable["checkOut"]);
-        echo("<br><br>");
+                    //show accommodation CheckIn and CheckOut Dates
+                    echo("Check In: ".$accomDetailsTable["checkIn"]);
+                    echo(" | ");
+                    echo("Check Out: ".$accomDetailsTable["checkOut"]);
+                    echo("<br><br>");
 
-        echo("<a href=../edit-trip/edit-accommodation.php?accoId=$accomRow[accoId]&tripId=$tripId>Edit Accommodation</a>");
-        echo("<br>");
+                    echo("<a href=../edit-trip/edit-accommodation.php?accoId=$accomRow[accoId]&tripId=$tripId>Edit Accommodation</a>");
+                    echo("<br>");
 ?>
 
 <hr>
 
 <?php
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SHOW TRANSPORTATION DETAILS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-        $transptIdTable = $transptIdTable -> fetch();
-        echo("<h3>");
-        echo("Travelling by: ".$transptIdTable["transId"]);
-        echo("</h3>");
-        echo("<br>");
-        echo("Booking ID: ".$transptIdTable["bookingId"]);
-        echo("<br><br>");
+                    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SHOW TRANSPORTATION DETAILS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+                    $transptIdTable = $transptIdTable -> fetch();
+                    echo("<h3>");
+                    echo("Travelling by: ".$transptIdTable["transId"]);
+                    echo("</h3>");
+                    echo("<br>");
+                    echo("Booking ID: ".$transptIdTable["bookingId"]);
+                    echo("<br><br>");
 
-        //show transportation CheckIn and CheckOut Dates
-        echo("Departure: ".$transptIdTable["checkIn"]);
-        echo(" | ");
-        echo("Arrival: ".$transptIdTable["checkOut"]);
-        echo("<br>");
+                    //show transportation CheckIn and CheckOut Dates
+                    echo("Departure: ".$transptIdTable["checkIn"]);
+                    echo(" | ");
+                    echo("Arrival: ".$transptIdTable["checkOut"]);
+                    echo("<br>");
 
-        echo("<a href=../edit-trip/edit-transportation.php?transId=$transptRow[transId]&tripId=$tripId>Edit Accommodation</a>");
-        echo("<br>");
-?>
-
+                    echo("<a href=../edit-trip/edit-transportation.php?transId=$transptRow[transId]&tripId=$tripId>Edit Transportation</a>");
+                    echo("<br>");
 ?>
 
 <hr>
 
 <?php
 
-        //show group members count
-        $usr_grTable = $pdo->prepare("SELECT COUNT(userId) as 'groups' FROM `users-groups` WHERE `tripId` = '$tripsRow[tripId]' "); 
-        $usr_grTable -> execute(); 
-        $usr_grTable = $usr_grTable ->fetch();
-        echo("<br><br>");
-        echo("Group members:  ");
-        echo ($usr_grTable['groups']);
-        echo("<a href='../group/group-home.php'> View Members</a>");
-        echo("<br><br>");
-        
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SHOW EVENTS DETAILS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+                    $eventsIdTable = $eventsIdTable -> fetch();
+                    echo("<h3>");
+                    echo("Events: ".$eventsIdTable["name"]);
+                    echo("</h3>");
+                    echo("<br>");
+                    echo("Address: ".$eventsIdTable["address"]);
+                    echo("<br><br>");
+                    echo("Check In: ".$eventsIdTable["checkIn"]);
+                    echo("Others: ".$eventsIdTable["others"]);
+                    echo("<br>");
+
+                    echo("<a href=../edit-trip/edit-transportation.php?transId=$eventsRow[eventId]&tripId=$tripId>Edit Event</a>");
+                    echo("<br>");
+?>
+
+<hr>
+
+<?php
+
+                    //show group members count
+                    $usr_grTable = $pdo->prepare("SELECT COUNT(userId) as 'groups' FROM `users-groups` WHERE `tripId` = '$tripsRow[tripId]' "); 
+                    $usr_grTable -> execute(); 
+                    $usr_grTable = $usr_grTable ->fetch();
+                    echo("<br><br>");
+                    echo("Group members:  ");
+                    echo ($usr_grTable['groups']);
+                    echo("<a href='../group/group-home.php'> View Members</a>");
+                    echo("<br><br>");
+                    
+                }
+?>
+
+<hr>
+
+<?php
+                echo('</div>');
+            }
+        }
     }
-?>
-
-<hr>
-
-<?php
-    echo('</div>');
-} 
+}
 ?>
